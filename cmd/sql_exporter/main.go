92f55bda066b15912079ea207b394c25314e5d56
nxcc <mlangner@noris.de>
2021-07-05 17:14:15 +0200

    u

--- cmd/sql_exporter/main.go
+++ cmd/sql_exporter/main.go
@@ -7,12 +7,13 @@ import (
 	"os"
 	"runtime"
 
-	"github.com/nxcc/sql_exporter"
+	_ "net/http/pprof"
+
 	log "github.com/golang/glog"
+	"github.com/nxcc/sql_exporter"
 	"github.com/prometheus/client_golang/prometheus"
 	"github.com/prometheus/client_golang/prometheus/promhttp"
 	"github.com/prometheus/common/version"
-	_ "net/http/pprof"
 )
 
 var (
